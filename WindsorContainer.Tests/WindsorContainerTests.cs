456a8245684aa3d2f70fb2372d035f249fa6403b
Robert Dresler <dresler.robert@gmail.com>
2017-06-20 08:53:34 +0200

    Tests for WindosorContainer.Release()

---
+++ WindsorContainer.Tests/WindsorContainerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Xunit;
+
+namespace WindsorContainer.Tests
+{
+    public class WindsorContainerTests
+    {
+        [Fact]
+        public void Release_ForTransientComponent_ShouldCallDisposeOnComponent()
+        {
+            var container = new Castle.Windsor.WindsorContainer();
+            container.Register(Component.For<IFoo>().ImplementedBy<Foo>().LifeStyle.Transient);
+            var foo = container.Resolve<IFoo>();
+            container.Release(foo);
+            Assert.Equal(Foo.NumberAfterDispose, foo.Number);
+        }
+
+        [Fact]
+        public void Release_ForSingletonComponent_ShouldNotCallDisposeOnComponent()
+        {
+            var container = new Castle.Windsor.WindsorContainer();
+            container.Register(Component.For<IFoo>().ImplementedBy<Foo>().LifeStyle.Singleton);
+            var foo = container.Resolve<IFoo>();
+            container.Release(foo);
+            Assert.Equal(Foo.NumberBeforeDispose, foo.Number);
+        }
+
+        interface IFoo : IDisposable
+        {
+            int Number { get; }
+        }
+
+        class Foo : IFoo
+        {
+            public static int NumberBeforeDispose = 42;
+            public static int NumberAfterDispose = -1;
+
+            public Foo()
+            {
+                Number = NumberBeforeDispose;
+            }
+
+            public int Number { get; private set; }
+
+            public void Dispose()
+            {
+                Number = NumberAfterDispose;
+            }
+        }
+    }
+}
